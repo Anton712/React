2bdce7d47d5b3679c97fef36e5484ee9e0f704ac
anton <anton.loncar@outlook.com>
2025-07-07 18:01:01 +0200

    passed custom arguments to event function

--- React essentials/src/App.jsx
+++ React essentials/src/App.jsx
@@ -4,8 +4,8 @@ import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
 
 function App() {
-  function handleSelect() {
-    console.log(`Tab selected - hello`);
+  function handleSelect(selectedButton) {
+    console.log("Tab selected - " + selectedButton);
   }
 
   return (
@@ -27,10 +27,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect("Components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("JSX")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
           </menu>
 
         </section>
