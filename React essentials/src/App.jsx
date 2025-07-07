a0a18bed8dd6d30d5a5bbffbcc4cde5d55ccf7cf
anton <anton.loncar@outlook.com>
2025-07-07 18:31:03 +0200

    added useState hook to change outputed content when button is clicked

--- React essentials/src/App.jsx
+++ React essentials/src/App.jsx
@@ -2,10 +2,18 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Please click a tab to see the content");
+  // useState is a React hook that allows you to add state to functional components.
+  // It returns an array with two elements: the current state value and a function to update
+
   function handleSelect(selectedButton) {
     console.log("Tab selected - " + selectedButton);
+    setSelectedTopic(selectedButton);
+    // console.log("Tab selected - " + selectedButton);
+    // if you log after the setSelectedTopic, it will still show the previous value because state updates are asynchronous.
   }
 
   return (
@@ -32,7 +40,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("Props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("State")}>State</TabButton>
           </menu>
-
+          {selectedTopic}
         </section>
       </main>
     </div>
