1e8cbade8bfd0967968232182a978041d2cc9794
anton <anton.loncar@outlook.com>
2025-07-08 17:33:49 +0200

    forwarded props to wrapped elements

--- React essentials/src/components/TabButton/TabButton.jsx
+++ React essentials/src/components/TabButton/TabButton.jsx
@@ -1,10 +1,10 @@
 import './TabButton.css';
 
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
 
 	return (
 		<li>
-			<button className={isSelected ? "active" : undefined} onClick={onSelect}>{children}</button>
+			<button className={isSelected ? "active" : undefined} {...props}>{children}</button>
 		</li>
 	);
 }
