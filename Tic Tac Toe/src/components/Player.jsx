9b8f8557add276b492a9efc6b463ca1bba54ef39
anton <anton.loncar@outlook.com>
2025-07-09 18:29:53 +0200

    added button caption to change when editing player name

--- Tic Tac Toe/src/components/Player.jsx
+++ Tic Tac Toe/src/components/Player.jsx
@@ -4,12 +4,14 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    // setIsEditing(!isEditing); // when updating value based on previous value state, you should use the function form of setState
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = isEditing
-    ? <input type="text" required></input>
+    ? <input type="text" required value={name}></input>
     : <span className="player-name">{name}</span>;
+    let buttonCaption = isEditing ? "Save" : "Edit";
 
   return (
     <li>
@@ -17,7 +19,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
     </li>
   );
 }
