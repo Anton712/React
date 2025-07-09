ae1fdc94bf1605c309339f5b9790c0e64f5936a0
anton <anton.loncar@outlook.com>
2025-07-09 11:20:52 +0200

    set component type inside Tabs component dynamically

--- React essentials/src/components/Examples.jsx
+++ React essentials/src/components/Examples.jsx
@@ -34,14 +34,18 @@ export default function Examples() {
 
 	return (
 		<Section id="examples" title="Examples">
-			<Tabs buttons={
-				<>
-					<TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect("components")}>Components</TabButton>
-					<TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect("jsx")}>JSX</TabButton>
-					<TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect("props")}>Props</TabButton>
-					<TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect("state")}>State</TabButton>
-				</>}>
-				{tabContent}</Tabs>
+			<Tabs
+				ButtonsContainer="menu" // use string for built in types, or use {} for custom components
+				buttons={
+					<>
+						<TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect("components")}>Components</TabButton>
+						<TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect("jsx")}>JSX</TabButton>
+						<TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect("props")}>Props</TabButton>
+						<TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect("state")}>State</TabButton>
+					</>
+				}
+			>{tabContent}
+			</Tabs>
 			<menu>
 
 			</menu>
--- React essentials/src/components/Tabs.jsx
+++ React essentials/src/components/Tabs.jsx
@@ -1,9 +1,14 @@
-export default function Tabs({ children, buttons }) {
+export default function Tabs({ children, buttons, ButtonsContainer }) {
+// const ButtonsContainer = buttonsContainer;
+// received component identifier as a prop
+// prop must be usable as a custom component in the receiving component, it must start with uppercase character,
+//  or must be remapped to variable that starts with uppercase character
+
 	return (
 		<>
-			<menu>
+			<ButtonsContainer>
 				{buttons}
-			</menu>
+			</ButtonsContainer>
 			{children}
 		</>
 	);
