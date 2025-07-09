8d6591e19e9fc7554f822714bc4c77b4d43808ac
anton <anton.loncar@outlook.com>
2025-07-09 18:38:29 +0200

    added 2-way binding to input field

--- Tic Tac Toe/src/components/Player.jsx
+++ Tic Tac Toe/src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -8,15 +9,20 @@ export default function Player({ name, symbol }) {
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = isEditing
-    ? <input type="text" required value={name}></input>
-    : <span className="player-name">{name}</span>;
+  function handleNameChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = isEditing
+    ? <input type="text" required value={playerName} onChange={handleNameChange}></input>
+    : <span className="player-name">{playerName}</span>;
     let buttonCaption = isEditing ? "Save" : "Edit";
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{buttonCaption}</button>
