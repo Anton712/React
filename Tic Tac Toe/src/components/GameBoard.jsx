79667ab7dcc326288383ff45925f3e495462de39
anton <anton.loncar@outlook.com>
2025-07-10 12:08:14 +0200

    updated gameboard state immutably so that buttons can be clicked

--- Tic Tac Toe/src/components/GameBoard.jsx
+++ Tic Tac Toe/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,25 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((previousBoard) => {
+      const updatedBoard = [...previousBoard.map(innerArray => [...innerArray])]; // create a copy of the board
+
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    })
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>))
             }
           </ol>
