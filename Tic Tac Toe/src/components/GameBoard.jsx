db469ab0968b34e602505963fbbafc6c851339c0
anton <anton.loncar@outlook.com>
2025-07-10 18:43:12 +0200

    disabled buttons if already selected

--- Tic Tac Toe/src/components/GameBoard.jsx
+++ Tic Tac Toe/src/components/GameBoard.jsx
@@ -4,7 +4,7 @@ const initialGameBoard = [
   [null, null, null]
 ];
 
-export default function GameBoard({onSelectSquare, turns}) {
+export default function GameBoard({ onSelectSquare, turns }) {
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   // function handleSelectSquare(rowIndex, colIndex) {
@@ -19,8 +19,8 @@ export default function GameBoard({onSelectSquare, turns}) {
 
   let gameBoard = initialGameBoard;
   for (const turn of turns) {
-    const {square, player } = turn;
-    const {row, col} = square;
+    const { square, player } = turn;
+    const { row, col } = square;
 
     gameBoard[row][col] = player;
   }
@@ -32,7 +32,10 @@ export default function GameBoard({onSelectSquare, turns}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}>
+                  {playerSymbol}
+                </button>
               </li>))
             }
           </ol>
