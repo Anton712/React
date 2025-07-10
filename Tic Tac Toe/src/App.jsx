c388c801add66b28ba6f987675effa0edc58dc97
anton <anton.loncar@outlook.com>
2025-07-10 18:40:22 +0200

    reduced state management & identified unnecessary state

--- Tic Tac Toe/src/App.jsx
+++ Tic Tac Toe/src/App.jsx
@@ -3,17 +3,23 @@ import GameBoard from "./components/GameBoard.jsx"
 import Player from "./components/Player.jsx"
 import Log from "./components/Log.jsx"
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  const currentPlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currentPlayer) => {
-      return currentPlayer === "X" ? "O" : "X";
-    });
-
     setGameTurns(previousTurns => {
-      let currentPlayer = "X";
+      let currentPlayer = deriveActivePlayer(previousTurns);
       if (previousTurns.length > 0 && previousTurns[0].player === "X") {
         currentPlayer = "O";
       }
@@ -37,11 +43,11 @@ function App() {
           <Player
             initialName="Player 1"
             symbol="X"
-            isActive={activePlayer === "X"} />
+            isActive={currentPlayer === "X"} />
           <Player
             initialName="Player 2"
             symbol="O"
-            isActive={activePlayer === "O"} />
+            isActive={currentPlayer === "O"} />
         </ol>
 
         <GameBoard
