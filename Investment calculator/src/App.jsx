96edc6f12928aaea851e403e244626d0b352cf1f
anton <anton.loncar@outlook.com>
2025-07-11 13:14:41 +0200

    created and build investment calculator app

---
+++ Investment calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import Result from "./components/Result.jsx"
+import UserInput from "./components/UserInput.jsx"
+
+function App() {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
