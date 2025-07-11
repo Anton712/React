e0e2dc7f78af6de8c72cc12f7ee445042be771db
anton <anton.loncar@outlook.com>
2025-07-11 13:22:48 +0200

    created Results component

---
+++ Investment calculator/src/components/Result.jsx
@@ -0,0 +1,33 @@
+export default function Result() {
+  return (
+    <>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Annual Contribution</th>
+            <th>Total Contributions</th>
+            <th>Interest Earned</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>$1,000.00</td>
+            <td>$100.00</td>
+            <td>$1,100.00</td>
+            <td>$0.00</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>$1,200.00</td>
+            <td>$100.00</td>
+            <td>$1,200.00</td>
+            <td>$0.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
