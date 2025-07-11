f588cdce08576df74977ec1ccb32a3463e08673d
anton <anton.loncar@outlook.com>
2025-07-11 11:23:04 +0200

    moved code to functions and improved code

--- Tic Tac Toe/src/App.jsx
+++ Tic Tac Toe/src/App.jsx
@@ -5,7 +5,12 @@ import Log from "./components/Log.jsx"
 import GameOver from "./components/GameOVer.jsx"
 import { WINNING_COMBINATIONS } from "./assets/winning-combinations.js"
 
-const initialGameBoard = [
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2"
+};
+
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
@@ -23,35 +28,42 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [players, setPlayers] = useState({
-    X: "Player 1",
-    O: "Player 2"
-  })
+  const [players, setPlayers] = useState(PLAYERS)
 
   const currentPlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = [...initialGameBoard.map(row => [...row])];
-  for (const turn of gameTurns) {
-    const { square, player } = turn;
-    const { row, col } = square;
+  function deriveGameBoard(gameTurns) {
+    let gameBoard = [...INITIAL_GAME_BOARD.map(row => [...row])];
+    for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
 
-    gameBoard[row][col] = player;
-  }
-
-  let winner = null;
-
-  for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
-    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
-
-    if (firstSquareSymbol
-      && firstSquareSymbol === secondSquareSymbol
-      && firstSquareSymbol === thirdSquareSymbol) {
-      winner = players[firstSquareSymbol];
+      gameBoard[row][col] = player;
     }
+
+    return gameBoard;
   }
 
+  function deriveWinner(gameBoard, players) {
+    let winner = null;
+
+    for (const combination of WINNING_COMBINATIONS) {
+      const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+      const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+      const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+      if (firstSquareSymbol
+        && firstSquareSymbol === secondSquareSymbol
+        && firstSquareSymbol === thirdSquareSymbol) {
+        winner = players[firstSquareSymbol];
+      }
+    }
+
+    return winner;
+  }
+
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -91,12 +103,12 @@ function App() {
       <div id="game-container">
         <ol id="players" className="highlight-player">
           <Player
-            initialName="Player 1"
+            initialName={PLAYERS.X}
             symbol="X"
             isActive={currentPlayer === "X"}
             onChangeName={handlePlayerNameChange} />
           <Player
-            initialName="Player 2"
+            initialName={PLAYERS.O}
             symbol="O"
             isActive={currentPlayer === "O"}
             onChangeName={handlePlayerNameChange} />
