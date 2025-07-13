8cea3d5c78279c1d4ff5b8d11a06878a04955a5c
anton <anton.loncar@outlook.com>
2025-07-13 14:30:32 +0200

    forwarded ref to custom child component

--- The Almost Final Countdown/src/components/ResultModal.jsx
+++ The Almost Final Countdown/src/components/ResultModal.jsx
@@ -1,5 +1,6 @@
-export default function ResultModal({result, targetTime}) {
-  return <dialog className="result-modal">
+
+export default function ResultModal({ref, result, targetTime }) {
+  return <dialog className="result-modal" ref={ref}>
     <h2>You {result}</h2>
     <p>The target time was <strong>{targetTime} seconds</strong></p>
     <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -8,3 +9,19 @@ export default function ResultModal({result, targetTime}) {
     </form>
   </dialog>
 }
+
+// old way of passing ref to child component before React 19
+// import {forwardRef} from 'react'
+
+// const ResultModal = forwardRef(function ResultModal({result, targetTime }, ref) {
+//   return <dialog className="result-modal" ref={ref}>
+//     <h2>You {result}</h2>
+//     <p>The target time was <strong>{targetTime} seconds</strong></p>
+//     <p>You stopped the timer with <strong>X seconds left.</strong></p>
+//     <form method="dialog">
+//       <button>Close</button>
+//     </form>
+//   </dialog>
+// })
+
+// export default ResultModal;
