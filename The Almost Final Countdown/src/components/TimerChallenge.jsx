078e0f8d7c624aebef67e4332225ea6d81618562
anton <anton.loncar@outlook.com>
2025-07-13 11:59:15 +0200

    added ResultModal component

--- The Almost Final Countdown/src/components/TimerChallenge.jsx
+++ The Almost Final Countdown/src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react";
+import ResultModal from "./ResultModal.jsx";
 
 
 
@@ -21,6 +22,8 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
+    <>
+    {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
     <section className="challenge">
       <h2>{title}</h2>
       {timerExpired && <p>You lost!</p>}
@@ -36,5 +39,6 @@ export default function TimerChallenge({ title, targetTime }) {
         {timerStarted ? "Time is running..." : "Timer incative"}
       </p>
     </section>
+    </>
   );
 }
