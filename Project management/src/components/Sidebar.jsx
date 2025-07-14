d8d721806788029363231c9cdb012b3a21db5577
anton <anton.loncar@outlook.com>
2025-07-14 14:49:56 +0200

    updated Sidebar component when project created

--- Project management/src/components/Sidebar.jsx
+++ Project management/src/components/Sidebar.jsx
@@ -1,6 +1,14 @@
 import Button from "./Button.jsx";
 
-export default function Sidebar({ onStartAddProject }) {
+export default function Sidebar({ onStartAddProject, projects }) {
+
+  const projectList = projects.map(project =>
+    <li key={project.id}>
+      <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+        {project.title}
+      </button>
+    </li>)
+
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
@@ -9,7 +17,9 @@ export default function Sidebar({ onStartAddProject }) {
           + Add Project
         </Button>
       </div>
-      <ul></ul>
+      <ul className="mt-8">
+        {projectList}
+      </ul>
     </aside>
   );
 }
