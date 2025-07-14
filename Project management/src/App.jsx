120c27a51eda2a25d41cf1ef6cc76f4fa65b06f6
anton <anton.loncar@outlook.com>
2025-07-14 12:49:43 +0200

    created NoProjectSelected component and Button component for reusability

--- Project management/src/App.jsx
+++ Project management/src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import Sidebar from "./components/Sidebar.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
