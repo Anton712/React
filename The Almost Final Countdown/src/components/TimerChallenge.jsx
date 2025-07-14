3eff42b53fd4ad6f9bbaf761710a41d2a90e553d
anton <anton.loncar@outlook.com>
2025-07-14 10:34:50 +0200

    shared state to ResultModal component so that timer is reset when modal is closed

--- The Almost Final Countdown/src/components/TimerChallenge.jsx
+++ The Almost Final Countdown/src/components/TimerChallenge.jsx
@@ -13,8 +13,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
     dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
@@ -30,21 +33,25 @@ export default function TimerChallenge({ title, targetTime }) {
 
   return (
     <>
-    <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
-    <section className="challenge">
-      <h2>{title}</h2>
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timerIsActive ? handleStop : handleStart}>
-          {timerIsActive ? "Stop" : "Start"} Challenge
-        </button>
-      </p>
-      <p className={timerIsActive ? 'active' : undefined}>
-        {timerIsActive ? "Time is running..." : "Timer incative"}
-      </p>
-    </section>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset} />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerIsActive ? 'active' : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer incative"}
+        </p>
+      </section>
     </>
   );
 }
