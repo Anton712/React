dfa97225b92bf8c64fb5586a2141ebf15058791a
anton <anton.loncar@outlook.com>
2025-07-15 11:12:06 +0200

    added project tasks

--- Project management/src/App.jsx
+++ Project management/src/App.jsx
@@ -7,8 +7,30 @@ import SelectedProject from "./components/SelectedProject.jsx";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  function handleAddTask(text) {
+    setProjectsState(prevState => {
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId
+      }
+
+      return {
+        ...prevState,
+        selectedProjectId:  prevState.selectedProjectId,
+        tasks: [...prevState.tasks, newTask]
+      }
+    })
+  }
+
+  function handleDeleteTask() {
+
+  }
 
   function handleDeleteProject() {
     setProjectsState(prevState => {
@@ -49,9 +71,10 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId
       }
 
       return {
@@ -64,7 +87,12 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  let content = <SelectedProject
+    project={selectedProject}
+    onDelete={handleDeleteProject}
+    onAddTask={handleAddTask}
+    onDeleteTask={handleDeleteTask}
+    tasks={projectsState.tasks} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
