d89c5b0623c9fbe594e6ddcc24862a776a915a2d
anton <anton.loncar@outlook.com>
2025-07-15 10:34:15 +0200

    handled project deletion

--- Project management/src/App.jsx
+++ Project management/src/App.jsx
@@ -10,6 +10,16 @@ function App() {
     projects: []
   });
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
+
   function handleSelectProject(id) {
     setProjectsState(prevState => {
       return {
@@ -54,7 +64,7 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject}/>;
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
