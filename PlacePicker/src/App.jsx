b1b6d261ebe847312a21a6f8678ccdc8f5cd91c3
anton <anton.loncar@outlook.com>
2025-07-22 11:42:27 +0200

    used useEffect to open and close modal, used useEffect cleanup function

--- PlacePicker/src/App.jsx
+++ PlacePicker/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     AVAILABLE_PLACES.find(place => place.id === id)
   );
 
-  const modal = useRef();
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   const selectedPlace = useRef();
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
@@ -41,12 +41,12 @@ function App() {
   }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -68,7 +68,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     localStorage.setItem(
@@ -79,7 +79,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
