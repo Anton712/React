3911cbef164a2593c4037d107f63f38fe98b4735
anton <anton.loncar@outlook.com>
2025-07-26 12:03:40 +0200

    getting user input via refs

--- React Forms/src/components/Login.jsx
+++ React Forms/src/components/Login.jsx
@@ -1,23 +1,14 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState("");
-  // const [enteredPassword, setEnteredPassword] = useState("");
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: ""
-  });
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(enteredValues);
-  }
-
-  function handleInputChange(identifier, value) {
-    setEnteredValues(prevValues => ({
-      ...prevValues,
-      [identifier]: value
-    }))
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+    console.log(enteredEmail, enteredPassword);
   }
 
   return (
@@ -31,18 +22,18 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
-            onChange={(event) => handleInputChange("email", event.target.value)}
-            value={enteredValues.email} />
+            ref={email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input 
-          id="password" 
-          type="password" 
-          name="password"
-          onChange={(event) => handleInputChange("password", event.target.value)}
-            value={enteredValues.password} />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            ref={password}
+           />
         </div>
       </div>
 
