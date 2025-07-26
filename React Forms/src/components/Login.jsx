0122014a56ff7c74c532f849d015ffce63e79299
anton <anton.loncar@outlook.com>
2025-07-26 12:52:56 +0200

    validating input upon form submission

--- React Forms/src/components/Login.jsx
+++ React Forms/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -8,7 +10,17 @@ export default function Login() {
     event.preventDefault();
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
-    console.log(enteredEmail, enteredPassword);
+    
+    const emailIsValid = enteredEmail.includes("@");
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true)
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log("Sending HTTP request...");
   }
 
   return (
@@ -24,6 +36,7 @@ export default function Login() {
             name="email"
             ref={email}
           />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
         </div>
 
         <div className="control no-margin">
