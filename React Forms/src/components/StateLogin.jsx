d59221fb40d799d901c82063cc6cd236a9e48f55
anton <anton.loncar@outlook.com>
2025-07-26 12:43:49 +0200

    validating input upon lost focus (blur)

--- React Forms/src/components/StateLogin.jsx
+++ React Forms/src/components/StateLogin.jsx
@@ -8,7 +8,12 @@ export default function Login() {
     password: ""
   });
 
-  const emailIsInvalid = enteredValues.email !== "" && !enteredValues.email.includes("@");
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -16,14 +21,26 @@ export default function Login() {
     setEnteredValues({
       email: "",
       password: ""
-    })
+    });
   }
 
   function handleInputChange(identifier, value) {
     setEnteredValues(prevValues => ({
       ...prevValues,
       [identifier]: value
-    }))
+    }));
+    
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false
+    }));
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true
+    }));
   }
 
   return (
@@ -37,20 +54,21 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email} />
-            <div className="control-error">
-              {emailIsInvalid && <p>Please enter a valid email address</p>}
-            </div>
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input 
-          id="password" 
-          type="password" 
-          name="password"
-          onChange={(event) => handleInputChange("password", event.target.value)}
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event.target.value)}
             value={enteredValues.password} />
         </div>
       </div>
