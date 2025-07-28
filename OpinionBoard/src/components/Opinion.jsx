0379254b6686acc88c97f719d8a43056aed660c9
anton <anton.loncar@outlook.com>
2025-07-28 19:15:48 +0200

    sent HTTP request via form action

--- OpinionBoard/src/components/Opinion.jsx
+++ OpinionBoard/src/components/Opinion.jsx
@@ -1,10 +1,16 @@
-export function Opinion({ opinion: { id, title, body, userName, votes } }) {
-  function upvoteAction() {
+import { use } from "react";
+import { OpinionsContext } from "../store/opinions-context";
 
+export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+  const { upvoteOpinion, downvoteOpinion } = use(OpinionsContext);
+
+
+  async function upvoteAction() {
+    await upvoteOpinion(id);
   }
 
-  function downwoteAction() {
-
+  async function downwoteAction() {
+    await downvoteOpinion(id);
   }
 
 
