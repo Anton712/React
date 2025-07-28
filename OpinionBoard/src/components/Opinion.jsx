a7ec12130f8d9d9b35b91018866998af03e76bb8
anton <anton.loncar@outlook.com>
2025-07-28 19:20:45 +0200

    used "pending" state from useActionState() hook to disable buttons

--- OpinionBoard/src/components/Opinion.jsx
+++ OpinionBoard/src/components/Opinion.jsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useActionState } from "react";
 import { OpinionsContext } from "../store/opinions-context";
 
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
@@ -13,6 +13,9 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
     await downvoteOpinion(id);
   }
 
+  const [upvoteFormState, upvoteFormAction, upvotePending] = useActionState(upvoteAction);
+  const [downwoteFormState, downwoteFormAction, downwotePending] = useActionState(downwoteAction);
+
 
   return (
     <article>
@@ -22,7 +25,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button formAction={upvoteAction}>
+        <button formAction={upvoteFormAction} disabled={upvotePending || downwotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -42,7 +45,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button formAction={downwoteAction}>
+        <button formAction={downwoteFormAction} disabled={upvotePending || downwotePending}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
