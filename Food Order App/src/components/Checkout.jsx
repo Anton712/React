4c6231f3f1df1b76d1af983ace96a7e62e91bcf4
anton <anton.loncar@outlook.com>
2025-07-29 15:31:07 +0200

    sent http post request with order data

--- Food Order App/src/components/Checkout.jsx
+++ Food Order App/src/components/Checkout.jsx
@@ -18,15 +18,35 @@ export default function Checkout() {
       userProgressCtx.hideCheckout();
     }
 
+    function handleSubmit(event) {
+      event.preventDefault();
+
+      const fd = new FormData(event.target);
+      const customerData = Object.fromEntries(fd.entries());
+
+      fetch("http://localhost:3000/orders", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+          order: {
+            items: cartCtx.items,
+            customer: customerData
+          }
+        })
+      })
+    }
+
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" id="full-name"/>
+        <Input label="Full Name" type="text" id="name"/>
         <Input label="E-Mail Address" type="email" id="email"/>
-        <Input label="Street" type="text" id="street"/>ć
+        <Input label="Street" type="text" id="street"/>
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code"/>
           <Input label="City" type="text" id="city"/>
